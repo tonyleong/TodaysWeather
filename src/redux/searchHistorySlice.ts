7079f6fd4a1463809a23d8aa8ae826138aabf5f4
Tony <59461665+tonyleong@users.noreply.github.com>
2024-03-27 14:06:13 +0800

    code format

--- src/redux/searchHistorySlice.ts
+++ src/redux/searchHistorySlice.ts
@@ -3,33 +3,36 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 import { WeatherApiState } from './searchWeatherSlice'
 
 export interface SearchHistoryState {
-    data: { [key: string]: WeatherApiState; }
+  data: { [key: string]: WeatherApiState; }
 }
 
 const initialState: SearchHistoryState = {
-    data: {},
+  data: {},
 }
 
 export const searchHistorySlice = createSlice({
-    name: 'weather',
-    initialState,
-    reducers: {
-        addSearchHistory: (state, action: PayloadAction<WeatherApiState>) => {
-            state.data[action.payload.id] = action.payload
-        }
+  name: 'weather',
+  initialState,
+  reducers: {
+    addSearchHistory: (state, action: PayloadAction<WeatherApiState>) => {
+      state.data[action.payload.id] = action.payload
     },
+    removeSearchHistory: (state, action: PayloadAction<string>) => {
+      delete state.data[action.payload]
+    }
+  },
 })
 
 export const selectSearchHistory = (state: RootState) => state?.searchHistory.data
 export const selectLatestSearchHistory = (state: RootState) => {
-    let { data } = state?.searchHistory
-    let latest = data[Object.keys(data)[0]]
-    Object.keys(data).forEach((key) => {
-        if (latest === null) latest = data[key]
-        else if (latest.timestamp < data[key].timestamp) latest = data[key]
-    })
-    return latest
+  let { data } = state?.searchHistory
+  let latest = data[Object.keys(data)[0]]
+  Object.keys(data).forEach((key) => {
+    if (latest === null) latest = data[key]
+    else if (latest.timestamp < data[key].timestamp) latest = data[key]
+  })
+  return latest
 
 }
-export const { addSearchHistory } = searchHistorySlice.actions
+export const { addSearchHistory, removeSearchHistory } = searchHistorySlice.actions
 export default searchHistorySlice.reducer
