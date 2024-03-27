8d89d4b52fffcbb4f008923c9dc269bbb44419b8
Tony <59461665+tonyleong@users.noreply.github.com>
2024-03-27 14:06:02 +0800

    remove history

--- src/components/historyItem.tsx
+++ src/components/historyItem.tsx
@@ -5,78 +5,84 @@ import { ThemeConfig } from "../themeConfig";
 import { WeatherApiState, useLazyGetWeatherQuery } from "../redux/searchWeatherSlice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { addSearchHistory } from "../redux/searchHistorySlice";
+import { addSearchHistory, removeSearchHistory } from "../redux/searchHistorySlice";
 import { CountryListType } from "./searchBar";
 import moment from "moment";
 
 export type HistoryItemProps = { data: WeatherApiState }
 
 const HistoryItem = ({ data }: HistoryItemProps) => {
-    const theme = useTheme();
-    const sm = useMediaQuery(theme.breakpoints.up('sm'));
-    const dispatch = useDispatch()
+  const theme = useTheme();
+  const sm = useMediaQuery(theme.breakpoints.up('sm'));
+  const dispatch = useDispatch()
 
-    const [getWeather, { data: searchData, isLoading, error }] = useLazyGetWeatherQuery()
-    useEffect(() => {
-        //TODO: check duplicate item add
-        if (searchData === null || searchData === undefined) return
-        console.log('history item triggered')
-        dispatch(addSearchHistory(searchData))
-    }, [searchData])
+  const [getWeather, { data: searchData, isLoading, error }] = useLazyGetWeatherQuery()
+  useEffect(() => {
+    if (searchData === null || searchData === undefined) return
+    dispatch(addSearchHistory(searchData))
+  }, [searchData])
 
-    const handleSearch = () => {
-        getWeather({
-            lon: data?.coord?.lon,
-            lat: data?.coord?.lat,
-            name: data?.name,
-            country: data?.sys?.country
-        })
-    }
-    return (
-        <Stack direction='row' gap={1.5} sx={{
-            width: '100%',
-            alignItems: "center",
-            backgroundColor: theme => ThemeConfig.historyItem[theme.palette.mode].background,
-            paddingX: '10px',
-            paddingY: '13px',
-            borderRadius: '16px',
+  const handleSearch = () => {
+    getWeather({
+      lon: data?.coord?.lon,
+      lat: data?.coord?.lat,
+      name: data?.name,
+      country: data?.sys?.country
+    })
+  }
+
+  const handleRemoveHistory = (id: string) => {
+    dispatch(removeSearchHistory(id))
+
+  }
+
+  return (
+    <Stack direction='row' gap={1.5} sx={{
+      width: '100%',
+      alignItems: "center",
+      backgroundColor: theme => ThemeConfig.historyItem[theme.palette.mode].background,
+      paddingX: '10px',
+      paddingY: '13px',
+      borderRadius: '16px',
+    }}>
+      <Stack direction={sm ? 'row' : 'column'} className="w-full">
+        <Typography sx={{
+          color: theme => ThemeConfig.historyItem[theme.palette.mode].primary,
+          fontSize: { xs: '14px', sm: '16px' }
         }}>
-            <Stack direction={sm ? 'row' : 'column'} className="w-full">
-                <Typography sx={{
-                    color: theme => ThemeConfig.historyItem[theme.palette.mode].primary,
-                    fontSize: { xs: '14px', sm: '16px' }
-                }}>
-                    {`${data?.name}, ${data?.sys?.country}`}
-                </Typography>
-                <Typography sx={{
-                    color: theme => ThemeConfig.historyItem[theme.palette.mode].secondary,
-                    fontSize: { xs: '10px', sm: '14px' },
-                    marginLeft: { xs: 0, sm: 'auto' }
-                }}>
-                    {moment(data?.timestamp).format('DD-MM-YYYY hh:mma')}
-                </Typography>
-            </Stack>
-            <IconButton sx={{
-                marginLeft: 'auto',
-                color: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.color,
-                border: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.border,
-                backgroundColor: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.backgroundColor
-            }}
-                onClick={handleSearch}
-                disabled={isLoading}
-            >
-                <SearchIcon sx={{ opacity: 0.5 }} />
-            </IconButton>
-            <IconButton sx={{
-                marginLeft: 'auto',
-                color: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.color,
-                border: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.border,
-                backgroundColor: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.backgroundColor
-            }}>
-                <DeleteIcon sx={{ opacity: 0.5 }} />
-            </IconButton>
-        </Stack>
-    )
+          {`${data?.name}, ${data?.sys?.country}`}
+        </Typography>
+        <Typography sx={{
+          color: theme => ThemeConfig.historyItem[theme.palette.mode].secondary,
+          fontSize: { xs: '10px', sm: '14px' },
+          marginLeft: { xs: 0, sm: 'auto' }
+        }}>
+          {moment(data?.timestamp).format('DD-MM-YYYY hh:mma')}
+        </Typography>
+      </Stack>
+      <IconButton sx={{
+        marginLeft: 'auto',
+        color: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.color,
+        border: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.border,
+        backgroundColor: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.backgroundColor
+      }}
+        onClick={handleSearch}
+        disabled={isLoading}
+      >
+        <SearchIcon sx={{ opacity: 0.5 }} />
+      </IconButton>
+      <IconButton sx={{
+        marginLeft: 'auto',
+        color: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.color,
+        border: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.border,
+        backgroundColor: theme => ThemeConfig.historyItem[theme.palette.mode].iconButton.backgroundColor
+      }}
+        onClick={() => handleRemoveHistory(data.id)}
+      >
+        <DeleteIcon sx={{ opacity: 0.5 }} />
+      </IconButton>
+    </Stack>
+  )
 }
 
 export default HistoryItem
